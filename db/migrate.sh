7adfe9df9e3d0aee69eb05cbd294b34b92943748
Ben Fields <me@benfields.net>
2013-12-08 11:34:22 +0000

    more table

--- db/migrate.sh
+++ db/migrate.sh
@@ -2,3 +2,4 @@
 
 sqlite3 db/app.db "CREATE TABLE echonest_responses (id integer primary key, json text);"
 sqlite3 db/app.db "CREATE TABLE upload (id integer primary key, file_name varchar(255));"
+sqlite3 db/app.db "CREATE TABLE timings (id integer, sync_method varchar(255), json text, PRIMARY KEY (id, sync_method));"
