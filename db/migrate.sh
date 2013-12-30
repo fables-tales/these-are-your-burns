1fe0f26e22d0eaf77bfded6b274866d5a0ed16b7
Sam Phippen <samphippen@googlemail.com>
2013-12-30 15:58:38 +0000

    Migrations with postgres

--- db/migrate.sh
+++ db/migrate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sqlite3 db/app.db "CREATE TABLE echonest_responses (id integer primary key, json text);"
-sqlite3 db/app.db "CREATE TABLE upload (id integer primary key, file_name varchar(255));"
-sqlite3 db/app.db "CREATE TABLE timings (id integer, sync_method varchar(255), json text, PRIMARY KEY (id, sync_method));"
+export PGPASSWORD="lol"
+psql --host=127.0.0.1 burns burns -c "CREATE TABLE echonest_responses (id integer primary key, json text);"
+psql --host=127.0.0.1 burns burns -c "CREATE TABLE upload (id integer primary key, file_name varchar(255));"
+psql --host=127.0.0.1 burns burns -c "CREATE TABLE timings (id integer, sync_method varchar(255), json text, PRIMARY KEY (id, sync_method));"
